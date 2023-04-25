2c767093c5c02a13b0553ec6990e40ef204ef008
gabovj <68244861+gabovj@users.noreply.github.com>
2023-04-25 15:35:41 -0500

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -14,34 +14,37 @@ ac_api_key = st.secrets['ACTIVE_CAMPAIGN_API']
 
 # Title of the app
 st.title("Generador de KPI's")
-
-# Position input field
-position = st.text_input("¿Cúal es el nombre de tu posción de trabajo?")
-
 # Company name input field
 company_name = st.text_input("¿Cúal es el nombre de tu empresa?")
-
-# Company size input field
-company_size = st.selectbox("¿Cúal es el tamaño de tu empresa?",
-                            ("Small", "Medium", "Large"))
-
-# Company sector input field
-company_sector = st.text_input("¿A que se dedica tu empresa?")
-
-# Name input field
-name = st.text_input("Ingresa tu nombre:")
-
 # Email input field
 email = st.text_input("Ingresa tu email:")
-
+# Name input field
+name = st.text_input("Ingresa tu nombre:")
+# Last Name input field
+last_name = st.text_input("Ingresa tu apellido:")
+# Position input field
+position = st.text_input("¿Cúal es el nombre de tu posción de trabajo?")
+# Company sector input field
+company_sector = st.text_input("¿A que se dedica tu empresa?")
+# management team size input field
+company_size = st.selectbox("¿Cúal es el tamaño de tu equipo administrativo?",
+                            ("1-5 personas", "6-15 personas", "21-70 personas", "71-250 personas", ">250 personas"))
+# annual income input field
+anual_income = st.selectbox("¿Cuantos años tiene operando tu empresa?",
+                            ("1-3 años", "4-7 años", "8-10 años", ">10 años"))
+# company years input field
+company_age = st.selectbox("¿Cúal es ingreso anual de tu empresa?",
+                           ("<1 MDD", "1-2 MDD", "2-5 MDD", "6-10 MDD", ">10 MDD"))
 # Button
-button = st.button("Generate KPIs")
-
+button = st.button("Generar KPIs")
 # Set up your API key
 openai.api_key = openai_api_key
 
 
 def search_contact_in_active_campaign(email):
+    """
+    Busca si existe ya el correo electronico en AC
+    """
     API_URL = f'https://neuva.api-us1.com/api/3/contacts?email={email}'
     headers = {
         "Api-Token": ac_api_key,
@@ -51,12 +54,30 @@ def search_contact_in_active_campaign(email):
     return response.json()
 
 
-def add_contact_to_active_campaign(name, email):
+def add_contact_to_active_campaign(name, last_name, email, size, years, income):
+    """
+    Agrega un nuevo contacto en AC
+    """
     API_URL = 'https://neuva.api-us1.com/api/3/contact/sync'
     data = {
         "contact": {
             "email": email,
             "firstName": name,
+            "lastName": last_name,
+            "fieldValues": [
+                {
+                    "field": "9",
+                    "value": size
+                },
+                {
+                    "field": "10",
+                    "value": years
+                },
+                {
+                    "field": "11",
+                    "value": income
+                },
+            ]
         }
     }
     headers = {
@@ -68,6 +89,9 @@ def add_contact_to_active_campaign(name, email):
 
 
 def update_contactlist(contact_id):
+    """
+    Agrega un contacto a la lista NEUVA de AC
+    """
     url = "https://neuva.api-us1.com/api/3/contactLists"
     payload = {"contactList": {
         "sourceid": 0,
@@ -132,19 +156,19 @@ else:
                 # Add contact to ActiveCampaign
                 list_id_neuva = 2
                 active_campaign_response = add_contact_to_active_campaign(
-                    name, email)
+                    name, last_name, email, company_size, company_age, anual_income)
                 # get contact ID
                 search_for_contact_id = search_contact_in_active_campaign(
                     email)
                 contact_id = search_for_contact_id["contacts"][0]['id']
                 active_campaign_update_contact = update_contactlist(contact_id)
                 st.write(contact_id)
-            with st.spinner('Generando KPIs...'):
+            with st.spinner(f'{name}, estamos generando tus KPIs...'):
                 response = openai.ChatCompletion.create(
                     model="gpt-3.5-turbo",
                     messages=[
                         {"role": "system", "content": "Eres un asesor de negocios."},
-                        {"role": "user", "content": f"Describe el propósito de un {position} de una empresa que se dedica a {company_sector}?"}
+                        {"role": "user", "content": f"Describe en maximo 20 palabras el propósito de un {position} de una empresa que se dedica a {company_sector}"}
                     ]
                 )
 
